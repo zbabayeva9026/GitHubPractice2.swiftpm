992207d4680ceee68c86ed4da41aa921c72a12ae
Zahra Babayeva <zbabayeva9026@stu.d214.org>
2026-04-15 10:40:04 -0500

    code

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number1: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+      
     }
 }
