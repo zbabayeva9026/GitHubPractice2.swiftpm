6f4a94fe89b979da74e03f8ee6b5b922358f415b
EJ Marshall <emarshall9247@stu.d214.org>
2026-04-15 10:44:19 -0500

    cpode

--- ContentView.swift
+++ ContentView.swift
@@ -3,10 +3,9 @@ import SwiftUI
 struct ContentView: View {
     @State var number1: Int = 0
     
-    @State var number2 = 1
+    @State var number2 = 0
     var body: some View {
       
-       
-    
+        TextField("number", value: $number1, format: .number)
     }
 }
