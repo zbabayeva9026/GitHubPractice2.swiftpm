65a52b613682f06bd8d90fe29ffd886f82ae5904
Zahra Babayeva <zbabayeva9026@stu.d214.org>
2026-04-15 10:52:49 -0500

    code

--- ContentView.swift
+++ ContentView.swift
@@ -6,18 +6,13 @@ struct ContentView: View {
     @State var number2 = 0
     var body: some View {
         TextField("number2", value: $number2, format: .number)
-        
-        
-        
-      
-           
-            
-
-    
-      
+     
         TextField("number", value: $number1, format: .number)
         Button("add") {
             answer = number1 + number2
+        }
+        Button("subtract") {
+            answer = number1 - number2
         }
         Text("Answer: \(answer)")
     }
